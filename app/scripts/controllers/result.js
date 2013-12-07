c1c6a1f72142532c617ee2380532ede703f789e2
Simon Fransson <simon@dessibelle.se>
2013-12-07 19:24:57 +0100

    Github pages.

--- app/scripts/controllers/result.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('hellbergApp')
-  .controller('ResultCtrl', ['$scope', '$location', '$routeParams', function($scope, $location, $routeParams) {
-    $scope.score = $routeParams.score;
-    $scope.answer = $routeParams.answer;
-  }]);
